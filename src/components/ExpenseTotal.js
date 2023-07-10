e1810f4b717a341ced615c0f272b7c110497a2b1
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-07-10 17:29:22 +0530

    Updated code

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,19 @@
+import { useContext } from "react"
+import { AppContext } from "../context/AppContext"
 
+
+export default function ExpenseTotal() {
+
+    const { expenses } = useContext(AppContext)
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost)
+    }, 0)
+
+    return (
+        <div className="alert alert-primary">
+            <span >
+                Spent so far: £{totalExpenses}
+            </span>
+        </div>
+    )
+}
